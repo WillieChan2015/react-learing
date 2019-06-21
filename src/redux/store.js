fa50e07f756d4ebc33c60f63a951ded817d0ffad
Willie Chen <willie.chen@jodoinc.com>
2019-06-21 09:49:17 +0800

    update:增加redux 模块热替换

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,4 +6,11 @@ import promiseMiddleware from './middleware/promiseMiddleware.js';
 
 const store = createStore(combineReducers, applyMiddleware(promiseMiddleware));
 
+if (module.hot) {
+    module.hot.accept("./reducers", () => {
+        const nextCombineReducers = require("./reducers").default;
+        store.replaceReducer(nextCombineReducers);
+    });
+}
+
 export default store;
